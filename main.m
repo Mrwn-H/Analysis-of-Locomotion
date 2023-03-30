8187bd3e737a87905581a2d8787519ff6bf4ef03
loicpachoud <126784836+loicpachoud@users.noreply.github.com>
2023-03-30 17:01:02 +0200

    Add files via upload

--- main.m
+++ main.m
@@ -11,7 +11,8 @@ uiwait(msgbox("Select the " + ...
 path = uigetdir;
 
 dataset = mount_data(path);
-
+dataset_malade=load("/Users/loic/Documents/EPFL/MA1/Locomotion/Project_1/data/Sci_dataset/DM002_TDM_08_2kmh.mat");
+dataset_malade.condition_1=dataset_malade.data;
 %% 2 Plot Data
 scale = 0; %emg signals scaled according to signal with biggest amplitude
 plot_dataset(dataset,scale);
@@ -25,12 +26,25 @@ events.L_events = L_events;
 events.R_events = R_events;
 L_events_t = L_events/120;
 R_events_t = R_events/120;
+%% Get kinematic malade
+%% 3 Get kinematic params
+cnd2 = dataset_malade.condition_1;
+kin_params2 = getKinematicParams(cnd2);
+Fs2 = cnd2.marker_sr;
+[L_events2,R_events2] = getEvents(kin_params2,Fs2);
+events2.L_events = L_events2;
+events2.R_events = R_events2;
+L_events_t2 = L_events2/120;
+R_events_t2 = R_events2/120;
 
 %% 4 Plot simulation
-plot_locomotion(cnd,events)
+%plot_locomotion(cnd2,events2)
 
-%%
+%% 5 Correlation
+gait_time_correlation=getAngleCorrelation(kin_params2,Fs2);
+figure(19)
+plot(gait_time_correlation.knee)
+figure(20)
+plot(gait_time_correlation.ankle)
 
-z = dataset.condition_2.LANK(:,3);
-figure
-plot(z)
+
