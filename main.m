82da5fc30e39d425863cd19e86b57001a41509a1
Mrwn-H <56805447+Mrwn-H@users.noreply.github.com>
2023-05-31 14:07:44 +0200

    Add files via upload

--- main.m
+++ main.m
@@ -17,12 +17,13 @@ all_events = getAllEvents(dataset,info);
 
 %% 3 Plot Data
 
-plot_params.scale = 0; %emg signals scaled according to signal with biggest amplitude
-plot_params.gait = 'cycle'; % 'cycle' = plot beginning of each cycle, 'swing' = plot left and right swings, 'none' = regular plot
+plot_params.scale = 1; %emg signals scaled according to signal with biggest amplitude
+plot_params.gait = 'none'; % 'cycle' = plot beginning of each cycle, 'swing' = plot left and right swings, 'none' = regular plot
+plot_params.envelope = 1;
 plot_dataset(dataset,plot_params,all_events);
 
 %% 4 Plot simulation
-plot_locomotion(dataset.condition_1,all_events.condition_1)
+plot_locomotion(dataset.condition_2,all_events.condition_2)
 
 %% 5 Correlation - A remanier pour la mise à jour
 gait_time_correlation=getAngleCorrelation(kin_params2,Fs2);
@@ -34,3 +35,10 @@ plot(gait_time_correlation.ankle)
 %% 6 Extract gait parameters
 
 kinematic_params = getKinematicParams(dataset,all_events);
+emg_params = getEMGParams(dataset,all_events);
+
+%%
+data = dataset.condition_1.LVLat;
+figure;
+plot(data);
+envelope(data,150,'peak');
